59348798216bfbf782aa73c58328ad1771256461
Noshin Raisa <156049642+noshinraisa24@users.noreply.github.com>
2025-05-30 15:58:31 -0400

    fixed small import issue in product_route

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, abort
 import os
 import requests
 from datetime import datetime, timedelta
